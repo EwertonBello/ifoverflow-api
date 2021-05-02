8cf07848e3b646e148f783c32e511310317c4c68
EwertonBello <ewerton.bello1212@gmail.com>
2021-05-02 01:13:04 -0300

    db query implemented in user.show

--- app/users/repository/user.py
+++ app/users/repository/user.py
@@ -8,4 +8,8 @@ def register(request: schemas.User, db: Session):
     return new_user
 
 def show(id: int, db: Session):
-    return f"Repository: User by id {id}"
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with the id {id} is not available")
+    return user
