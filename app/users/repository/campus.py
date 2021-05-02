73701564f43b4dcc2a484e792d972376aa0d4911
EwertonBello <ewerton.bello1212@gmail.com>
2021-05-02 01:19:30 -0300

    db query implemented in campus.show and campus.get_all

--- app/users/repository/campus.py
+++ app/users/repository/campus.py
@@ -1,12 +1,19 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
 
+from app.users.models import Campus
+
 
 def get_all(db: Session):
-    return "Repository: List of Campus"
+    campus_list = db.query(Campus).all()
+    return campus_list
 
 def show(id: int, db: Session):
-    return f"Repository: Campus {id}"
+	campus = db.query(Campus).filter(Campus.id == id).first()
+    if not campus:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Campus with the id {id} is not available")
+    return campus
 
 def users_by_campus(id: int, db: Session):
     return f"Repository: Users of campus {id}"
