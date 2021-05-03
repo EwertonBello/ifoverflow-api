cbda6c200bafe4eb21ce1fc873747840ee5896d9
EwertonBello <ewerton.bello1212@gmail.com>
2021-05-03 01:10:53 -0300

    update question and category routers with new repository

--- app/questions/routers/category.py
+++ app/questions/routers/category.py
@@ -2,7 +2,7 @@ from typing import List
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends
-# from app.questions.repository import category
+from app.questions.repository import category
 # from app.questions.schemas import ShowCategory, ShowCategoryWithQuestions
 from app import database
 
@@ -15,13 +15,13 @@ get_db = database.get_db
 
 @router.get('/')
 async def get_categories(db: Session = Depends(get_db)):
-    return "category.get_all(db)"
+    return category.get_all(db)
 
 @router.get('/{id}')
 async def get_category(id: int, db: Session = Depends(get_db)):
-    return "category.show(id, db)"
+    return category.show(id, db)
 
 @router.get('/{id}/questions')
 async def get_questions_by_category(id: int, db: Session = Depends(get_db)):
-    return "category.questions_by_category(id, db)"
+    return category.questions_by_category(id, db)
 
