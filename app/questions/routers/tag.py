8d9238defe1dc5f252908d3ef7efb0af09e8d85d
EwertonBello <ewerton.bello1212@gmail.com>
2021-05-03 01:05:54 -0300

    update tag router with new repository

--- app/questions/routers/tag.py
+++ app/questions/routers/tag.py
@@ -2,7 +2,7 @@ from typing import List
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends
-# from app.questions.repository import tag
+from app.questions.repository import tag
 # from app.questions.schemas import ShowTag, ShowTagWithQuestions
 from app import database
 
@@ -15,8 +15,8 @@ get_db = database.get_db
 
 @router.get('/')
 async def get_tags(db: Session = Depends(get_db)):
-    return "tag.get_all(db)"
+    return tag.get_all(db)
 
 @router.get('/{id}/questions')
 async def get_questions_by_tag(id: int, db: Session = Depends(get_db)):
-    return "tag.questions_by_tag(id, db)"
+    return tag.questions_by_tag(id, db)
