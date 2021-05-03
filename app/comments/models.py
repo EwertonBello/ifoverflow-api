84f755e79e81061ff388aa87f8187d9177d1afc8
EwertonBello <ewerton.bello1212@gmail.com>
2021-05-03 20:38:48 -0300

    models of comments context created

---
+++ app/comments/models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, ForeignKey, INT, TEXT
+from app.database import Base
+from sqlalchemy.orm import relationship
+
+
+class Comments_Question(Base):
+    __tablename__ = 'Comentarios_Pergunta'
+
+    id = Column('id_comentarios_pergunta', INT, primary_key=True)
+    description = Column('descricao', TEXT(1000))
+    # --- belongsTo ---
+    user_id = Column('Usuarios_id_usuario', INT, ForeignKey('Usuarios.id_usuario'))
+    user = relationship("User", back_populates="comments")
+
+    question_id = Column('Perguntas_id_pergunta', INT, ForeignKey('Perguntas.id_pergunta'))
+    question = relationship("Question", back_populates="comments")
+    # --- belongsTo ---
+
+class Comments_Answer(Base):
+    __tablename__ = 'Comentarios_Resposta'
+
+    id = Column('id_comentarios_resposta', INT, primary_key=True)
+    description = Column('descricao', TEXT(1000))
+    # --- belongsTo ---
+    user_id = Column('Usuarios_id_usuario', INT, ForeignKey('Usuarios.id_usuario'))
+    user = relationship("User", back_populates="comments")
+
+    answer_id = Column('Respostas_id_resposta', INT, ForeignKey('Respostas.id_resposta'))
+    answer = relationship("Answer", back_populates="comments")
+    # --- belongsTo ---
