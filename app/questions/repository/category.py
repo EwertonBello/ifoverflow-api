6e5f332e6ee8484d7442f8707e1ac0a5fb50a7e9
EwertonBello <ewerton.bello1212@gmail.com>
2021-05-03 01:43:57 -0300

    update repository category

--- app/questions/repository/category.py
+++ app/questions/repository/category.py
@@ -5,12 +5,10 @@ from app.questions.models import Category
 
 
 def get_all(db: Session):
-    return "get_all"
     categories = db.query(Category).all()
     return categories
 
 def show(id: int, db: Session):
-    return "show"
     _category = db.query(Category).filter(Category.id == id).first()
     if not _category:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -18,7 +16,6 @@ def show(id: int, db: Session):
     return _category
 
 def questions_by_category(id: int, db: Session):
-    return "questions_by_category"
     _category = db.query(Category).filter(Category.id == id).first()
     if not _category:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
