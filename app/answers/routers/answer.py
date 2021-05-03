4b56f8cfba2de9408f559e6be9d2a1afefb28358
EwertonBello <ewerton.bello1212@gmail.com>
2021-05-03 17:46:29 -0300

    fix import router answer

--- app/answers/routers/answer.py
+++ app/answers/routers/answer.py
@@ -2,7 +2,7 @@ from typing import List
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends
-from app.answers.repository import answers
+from app.answers.repository import answer
 from app.answers.schemas import ShowAnswer, Answer
 from app.users.schemas import User
 from app.users import oauth2
@@ -17,16 +17,16 @@ get_db = database.get_db
 
 @router.post('/', status_code=201)
 async def create_answer(request: Answer, db: Session = Depends(get_db)):
-    return answers.create(request, db)
+    return answer.create(request, db)
 
 @router.get('/{id}', response_model=ShowAnswer)
 async def get_answer(id: int, db: Session = Depends(get_db)):
-    return answers.show(id, db)
+    return answer.show(id, db)
 
 @router.put('/{id}')
 async def vote_answer(id: int, db: Session = Depends(get_db)):
-    return answers.vote_answer(id, db)
+    return answer.vote_answer(id, db)
 
 @router.put('/{id}')
 async def accept_answer(id: int, db: Session = Depends(get_db), current_user: User = Depends(oauth2.get_current_user)):
-    return answers.accept_answer(current_user.id, id, db)
+    return answer.accept_answer(current_user.id, id, db)
