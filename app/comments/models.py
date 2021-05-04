53073fce29980391322b6863dd1f2de8ea87e7d9
EwertonBello <ewerton.bello1212@gmail.com>
2021-05-03 22:25:57 -0300

    added relatioship of comments in User

--- app/comments/models.py
+++ app/comments/models.py
@@ -10,7 +10,7 @@ class Comments_Question(Base):
     description = Column('descricao', TEXT(1000))
     # --- belongsTo ---
     user_id = Column('Usuarios_id_usuario', INT, ForeignKey('Usuarios.id_usuario'))
-    user = relationship("User", back_populates="comments")
+    user = relationship("User", back_populates="comments_question")
 
     question_id = Column('Perguntas_id_pergunta', INT, ForeignKey('Perguntas.id_pergunta'))
     question = relationship("Question", back_populates="comments")
@@ -23,7 +23,7 @@ class Comments_Answer(Base):
     description = Column('descricao', TEXT(1000))
     # --- belongsTo ---
     user_id = Column('Usuarios_id_usuario', INT, ForeignKey('Usuarios.id_usuario'))
-    user = relationship("User", back_populates="comments")
+    user = relationship("User", back_populates="comments_answer")
 
     answer_id = Column('Respostas_id_resposta', INT, ForeignKey('Respostas.id_resposta'))
     answer = relationship("Answer", back_populates="comments")
