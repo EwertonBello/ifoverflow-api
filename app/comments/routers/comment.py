aea3b350a169c66630e01c2b4f5a7e9df5b67602
EwertonBello <ewerton.bello1212@gmail.com>
2021-05-03 22:14:44 -0300

    update with new repository

--- app/comments/routers/comment.py
+++ app/comments/routers/comment.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends
-# from app.comments.repository import comment
+from app.comments.repository import comment
 from app.comments.schemas import ShowCommentAnswer, ShowCommentQuestion, CommentAnswer, CommentQuestion
 from app.users.schemas import User
 from app.users import oauth2
@@ -16,16 +16,16 @@ get_db = database.get_db
 
 @router.post('/answer', status_code=201)
 async def comment_answer(request: CommentAnswer, db: Session = Depends(get_db), current_user: User = Depends(oauth2.get_current_user)):
-    return "comment.comment_answer(current_user.id, request, db)"
+    return comment.comment_answer(current_user.id, request, db)
 
 @router.post('/question', status_code=201)
 async def comment_question(request: CommentQuestion, db: Session = Depends(get_db), current_user: User = Depends(oauth2.get_current_user)):
-    return "comment.comment_question(current_user.id, request, db)"
+    return comment.comment_question(current_user.id, request, db)
 
 @router.get('/{id}/answer', response_model=ShowCommentAnswer)
 async def get_comment_answer(id: int, db: Session = Depends(get_db)):
-    return "comment.show(False, id, db)"
+    return comment.show(False, id, db)
 
 @router.get('/{id}/question', response_model=ShowCommentQuestion)
 async def get_comment_question(id: int, db: Session = Depends(get_db)):
-    return "comment.show(True, id, db)"
+    return comment.show(True, id, db)
